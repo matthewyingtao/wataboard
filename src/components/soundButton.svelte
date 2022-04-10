84936a07a282bcd8c84c6827abb55b007b753b53
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-04-10 21:51:18 +1200

    initial commit

---
+++ src/components/soundButton.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { Howl } from 'howler';
+
+	export let sound;
+
+	const soundEl = new Howl({
+		src: [`/sounds/${sound}`],
+		preload: true
+	});
+</script>
+
+<button
+	on:click={() =>
+		// @ts-ignore
+		soundEl.play()}
+>
+	{sound}
+</button>
+
+<style>
+	button {
+		flex-grow: 1;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { sounds } from '../data/sounds.js';
+	import SoundButton from '../components/soundButton.svelte';
+</script>
+
+<main>
+	<h1>Wataboard</h1>
+
+	<div class="sound-grid">
+		{#each sounds as sound}
+			<SoundButton {sound} />
+		{/each}
+	</div>
+</main>
+
+<style>
+	main {
+		max-width: 55rem;
+		margin: 2rem auto;
+	}
+
+	h1 {
+		text-align: center;
+		margin-bottom: 2rem;
+	}
+
+	.sound-grid {
+		display: flex;
+		flex-wrap: wrap;
+		grid-gap: 1rem;
+	}
+</style>

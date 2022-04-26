14350ed44b88aa776c8f15df2357d4549bf1182b
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-04-26 21:05:09 +1200

    further styled buttons

--- src/components/SoundGrid.svelte
+++ src/components/SoundGrid.svelte
@@ -6,7 +6,11 @@
 
 {#each sounds as section}
 	<section>
-		<h2>{$lang == 'EN' ? section.title.en : section.title.jp}</h2>
+		<div class="heading">
+			<img class="penlights" src="/penlights.png" alt="" />
+			<h2>{$lang == 'EN' ? section.title.en : section.title.jp}</h2>
+			<img class="penlights" src="/penlights.png" alt="" />
+		</div>
 		<div class="sound-grid">
 			{#each section.sounds as sound}
 				<SoundButton {sound} language={$lang} />
@@ -19,6 +23,16 @@
 	h2 {
 		font-size: 2.5rem;
 		margin-bottom: 1rem;
+	}
+
+	.heading {
+		display: flex;
+		gap: 0.5rem;
+	}
+
+	.penlights {
+		margin-top: 0.5rem;
+		height: 2.5rem;
 	}
 
 	.sound-grid {
--- src/components/soundButton.svelte
+++ src/components/soundButton.svelte
@@ -4,7 +4,7 @@
 	export let sound;
 	export let language;
 
-	$: soundName = language == "EN" ? sound.en : sound.jp;
+	$: soundName = language == 'EN' ? sound.en : sound.jp;
 
 	const soundEl = new Howl({
 		src: [`/sounds/${sound.file}`],
@@ -24,15 +24,42 @@
 	}}
 >
 	{soundName}
+	<img class="glasses" src="/watamePartyGlasses.png" alt="" />
 </button>
 
 <style>
 	button {
+		position: relative;
 		display: block;
-		background: white;
+		cursor: pointer;
+		background: linear-gradient(to bottom, #fff4d6, #ffe382);
 		border: 1px solid #000b42;
 		border-radius: 999px;
-		padding: 0.5rem 1rem;
+		padding: 0.5rem 2rem;
 		flex-grow: 1;
+		transition: transform 150ms ease-out;
+	}
+
+	.glasses {
+		position: absolute;
+		top: -0.25rem;
+		right: -0.75rem;
+		width: 3.25rem;
+		transform: rotate(10deg);
+		opacity: 0;
+		transition: opacity 150ms ease-out;
+	}
+
+	button:hover {
+		transform: translateY(-2px);
+	}
+
+	button:hover .glasses {
+		opacity: 1;
+	}
+
+	button:active {
+		transition: transform 50ms ease-out;
+		transform: translateY(2px);
 	}
 </style>

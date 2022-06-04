023896fbcfb18c5a549c23929377cb22b8e570ce
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-06-05 08:31:17 +1200

    replaced placeholder sounds

--- src/data/sounds.js
+++ src/data/sounds.js
@@ -1,419 +1,420 @@
 export const sounds = [
 	{
-		title: {
-			en: 'Catch Phrases',
-			jp: 'キャッチ・フレーゼズ'
-		},
 		sounds: [
 			{
-				en: '00_pkmn_EN',
-				jp: '00_pkmn_JP',
-				file: '00_pkmn.mp3'		
+				file: 'aaaagh.mp3',
+				en: 'Aaaagh',
+				jp: 'あ゙あ゙あ゙あ゙あ゙あ゙'
 			},
 			{
-				en: '01_a-a-a-a_EN',
-				jp: '01_a-a-a-a_JP',
-				file: '01_a-a-a-a.mp3'
+				file: 'Abayo.mp3',
+				en: 'Farewell',
+				jp: 'ｱﾊﾞﾖ'
 			},
 			{
-				en: '01_pkmn_EN',
-				jp: '01_pkmn_JP',
-				file: '01_pkmn.mp3'
+				file: 'araara.mp3',
+				en: 'Ara ara',
+				jp: 'あらあら～'
 			},
 			{
-				en: '02_aaa-a-a-a_a-a-a_a-a-a_aa_EN',
-				jp: '02_aaa-a-a-a_a-a-a_a-a-a_aa_JP',
-				file: '02_aaa-a-a-a_a-a-a_a-a-a_aa.mp3'
+				file: 'BaingBaing.mp3',
+				en: 'Baing Baing',
+				jp: 'バーインバーイン'
 			},
 			{
-				en: '02_pkmn.EN',
-				jp: '02_pkmn_JP',
-				file: '02_pkmn.mp3'
+				file: 'Beautiful.mp3',
+				en: 'Beautiful',
+				jp: 'ビューティフル'
 			},
 			{
-				en: '03_a-a-a-A_EN',
-				jp: '03_a-a-a-A_JP',
-				file: '03_a-a-a-A.mp3'
+				file: 'Check_it_out.mp3',
+				en: 'Check it out!',
+				jp: 'チェケラ！'
 			},
 			{
-				en: '03_pkmn_EN',
-				jp: '03_pkmn_JP',
-				file: '03_pkmn.mp3'
+				file: 'Cut.mp3',
+				en: 'Cut',
+				jp: 'カット！'
 			},
 			{
-				en: '1-kousui1 (laugh) EN',
-				jp: '1-kousui1 (laugh) JP',
-				file: '1-kousui1 (laugh).mp3'
+				file: 'cute_sound.mp3',
+				en: 'Yochi yochi',
+				jp: 'よちよち'
 			},
 			{
-				en: '10-shion_duoooooooon_EN',
-				jp: '10-shion_duoooooooon_JP',
-				file: '10-shion_duoooooooon.mp3'
+				file: 'DontAbandonMe.mp3',
+				en: "Don't abandon me...！",
+				jp: '捨てないでおねがい！'
 			},
 			{
-				en: '100_sennou_silent_EN',
-				jp: '100_sennou_silent_JP',
-				file: '100_sennou_silent.mp3'
+				file: 'g_g_e_z.mp3',
+				en: 'GG EZ(ed)',
+				jp: 'ジージー　イーゼッド'
 			},
 			{
-				en: '11-shion_kara_no_ai_dayo_EN',
-				jp: '11-shion_kara_no_ai_dayo_JP',
-				file: '11-shion_kara_no_ai_dayo.mp3'
+				file: 'GaHaHa.mp3',
+				en: 'GaHaHa！',
+				jp: 'ガハハハッ'
 			},
 			{
-				en: '12-shion kiss_EN',
-				jp: '12-shion kiss_JP',
-				file: '12-shion kiss.mp3'
+				file: 'gesundheit.mp3',
+				en: 'Gesundheit',
+				jp: '（？）'
 			},
 			{
-				en: '13-shion llama short_EN',
-				jp: '13-shion llama short_JP',
-				file: '13-shion llama short.mp3'
+				file: 'gg_ez.mp3',
+				en: 'GGEZ',
+				jp: 'グッドゲーム　イージー'
 			},
 			{
-				en: '14-shion llama_EN',
-				jp: '14-shion llama_JP',
-				file: '14-shion llama.mp3'
+				file: 'headpat.mp3',
+				en: 'Headpaaat',
+				jp: 'ヘッドパーット'
+			},
+			{
+				file: 'HelpmeHelpme.mp3',
+				en: 'Help meee',
+				jp: 'ヘルプミー'
+			},
+			{
+				file: 'Hey_guys.mp3',
+				en: 'Hey guys',
+				jp: 'ヘイガーイズ'
+			},
+			{
+				file: 'hi_honey.mp3',
+				en: 'Hi honey',
+				jp: 'ハイハニー～'
+			},
+			{
+				file: 'I_like_sand.mp3',
+				en: 'I like sand',
+				jp: '砂が好き（英語）'
+			},
+			{
+				file: 'iamsheep.mp3',
+				en: 'I am sheep!',
+				jp: 'アイアムシープ！'
+			},
+			{
+				file: 'IDontWannaBeJingiskanAnymore.mp3',
+				en: "Don't want to be jingisukan",
+				jp: 'ジンギスカンになりたくない'
+			},
+			{
+				file: 'Imhungry.mp3',
+				en: "I'm hungry",
+				jp: 'アイムハングリー'
+			},
+			{
+				file: 'kachi.mp3',
+				en: 'kachi',
+				jp: 'ｶﾁｯ'
+			},
+			{
+				file: 'kao.mp3',
+				en: 'Angry about big face jokes',
+				jp: '顔デカくねって（激おこ）'
+			},
+			{
+				file: 'lets_go.mp3',
+				en: "Let's goo",
+				jp: 'レッツゴー'
+			},
+			{
+				file: 'longtone.mp3',
+				en: 'eeeee??? (long)',
+				jp: 'ええええ？？（長い）'
+			},
+			{
+				file: 'Mario.mp3',
+				en: 'Mario',
+				jp: 'マリオ'
+			},
+			{
+				file: 'meeeeh.mp3',
+				en: 'Meeeehhhh',
+				jp: 'メﾞｴﾞｴﾞｴﾞｴﾞ'
+			},
+			{
+				file: 'meshiequalfood.mp3',
+				en: 'Meshi = Food',
+				jp: '飯＝フード'
+			},
+			{
+				file: 'milk_for_you_bebi.mp3',
+				en: 'Milk for you baby',
+				jp: 'ミルクフォーユーベビー'
+			},
+			{
+				file: 'MyNameIsWatameTsunomaki.mp3',
+				en: 'My name is Watame Tsunomaki',
+				jp: 'マイネームイズわため角巻'
+			},
+			{
+				file: 'nadenade.mp3',
+				en: 'nade nade',
+				jp: 'なでなで'
+			},
+			{
+				file: 'Ohmygosh.mp3',
+				en: 'Oh my gosh!',
+				jp: 'オーマイガッシュ！'
+			},
+			{
+				file: 'oiiii.mp3',
+				en: 'Oii!',
+				jp: 'おおぃ！'
+			},
+			{
+				file: 'omaewa.mp3',
+				en: 'You are already dead',
+				jp: 'お前はもう死んでいる'
+			},
+			{
+				file: 'PekoLaugh.mp3',
+				en: 'Peko laugh',
+				jp: 'ぺこ笑い'
+			},
+			{
+				file: 'Pikachu.mp3',
+				en: 'Pikachu impression',
+				jp: 'ピカチュウ真似'
+			},
+			{
+				file: 'PrettyKetsu.mp3',
+				en: 'WOW Pretty Butt',
+				jp: 'わお！プリティーケツ'
+			},
+			{
+				file: 'RepeatAfterMeTasukete.mp3',
+				en: 'Repeat after me: Help me',
+				jp: 'リピートアフターミー「助けて」'
+			},
+			{
+				file: 'ShigureUii.mp3',
+				en: "I'm Shigure Ui",
+				jp: 'しぐれういでーす'
+			},
+			{
+				file: 'TowasamaIsDevil.mp3',
+				en: '”Towasama is devil”',
+				jp: 'トワ様イズデヴィル'
+			},
+			{
+				file: 'Tsundere_Watame_Calls_You_Baka.mp3',
+				en: 'Tsundere(?) Watame insults you',
+				jp: 'ツンデレ(？)わための罵倒'
+			},
+			{
+				file: 'uwaaaaaa.mp3',
+				en: 'Uwaaaaaa',
+				jp: 'うわああぁ'
+			},
+			{
+				file: 'wao.mp3',
+				en: 'Wao',
+				jp: 'わお。'
+			},
+			{
+				file: 'WataBeam.mp3',
+				en: 'Wata Beam',
+				jp: 'わたビーム'
+			},
+			{
+				file: 'watame_assures_that_she_is_safe_for_children_to_watch.mp3',
+				en: 'Watame is safe for children',
+				jp: '子供が見ても大丈夫です'
+			},
+			{
+				file: 'watame_brainwash.mp3',
+				en: 'Watame Brainwashing',
+				jp: 'わため洗脳'
+			},
+			{
+				file: 'watame_chu_kachi.mp3',
+				en: 'Thank you kiss',
+				jp: '感謝のチュ'
+			},
+			{
+				file: 'watame_favourite_word.mp3',
+				en: "Watame's Favorite Word",
+				jp: 'わためぇの好きな言葉'
+			},
+			{
+				file: 'watame_good_night_good_boy_bye_v2.mp3',
+				en: 'Good night good boy',
+				jp: 'グナイ～グッボイ～'
+			},
+			{
+				file: 'watame_happy_sheep_v2.mp3',
+				en: 'Happy sheep',
+				jp: 'ハッピーシープ'
+			},
+			{
+				file: 'watame_im_back.mp3',
+				en: "I'm back",
+				jp: 'ただいまー'
+			},
+			{
+				file: 'watame_kick.mp3',
+				en: 'Watame kick!',
+				jp: 'わためキーック'
+			},
+			{
+				file: 'watame_koko_is_doko.mp3',
+				en: "Where's this? Koko is doko?",
+				jp: 'ここイズどこ？'
+			},
+			{
+				file: 'watame_kora.mp3',
+				en: 'Kora!',
+				jp: 'コラ！コラ！'
+			},
+			{
+				file: 'watame_lava_scream.mp3',
+				en: 'Shriek (Lava)',
+				jp: '悲鳴（マグマ）'
+			},
+			{
+				file: 'Watame_love_you_all_kachi.mp3',
+				en: 'Love you all! Mwa!',
+				jp: '愛してんでぇ！んま！'
+			},
+			{
+				file: 'watame_mental_physical_vital_oriental.mp3',
+				en: 'Mental, Physical, Vital, Oriental',
+				jp: 'メンタル、フィジカル、バイタル、オリエンタル'
+			},
+			{
+				file: 'watame_nande.mp3',
+				en: 'Ahhh, why!?',
+				jp: 'あああ、なんで！？'
+			},
+			{
+				file: 'watame_nom_2.mp3',
+				en: 'mogu mogu',
+				jp: 'モグモグ'
+			},
+			{
+				file: 'watame_screams_from_failure.mp3',
+				en: 'Aaaagggh, kuso!',
+				jp: 'あああああ、クソー！'
+			},
+			{
+				file: 'watame_thinks_she_did_it_but_then_realizes.mp3',
+				en: "I did it! ...no I didn't",
+				jp: 'やったー！...やってない'
+			},
+			{
+				file: 'watame_very_very_hungry.mp3',
+				en: 'Very very hungry',
+				jp: 'ベリーベリーハングリー'
+			},
+			{
+				file: 'watame_voice_1_warukunai.mp3',
+				en: 'Watame did nothing wrong',
+				jp: 'わためは悪くないよねぇ？'
+			},
+			{
+				file: 'watame_wow.mp3',
+				en: 'Wow',
+				jp: 'わーお'
+			},
+			{
+				file: 'WatameFailure.mp3',
+				en: 'Failure teaches success.',
+				jp: '失敗は成功のもと（英語）'
+			},
+			{
+				file: 'WatameNumber1.mp3',
+				en: 'Watame No.1',
+				jp: 'わためNo.1'
+			},
+			{
+				file: 'WhosAGoat.mp3',
+				en: "Who's a goat!?",
+				jp: '誰がヤギや'
+			},
+			{
+				file: 'WhosAnOjisan.mp3',
+				en: "Who's an ojisan!?",
+				jp: 'クソー！誰がおじさんやー'
+			},
+			{
+				file: 'WTM_everyone_is_my_important_family_kachi.mp3',
+				en: 'Everyone is my important family',
+				jp: '皆が私の大切な家族だ（英語）'
+			},
+			{
+				file: 'YouDidNotSeeAnything.mp3',
+				en: 'Everyone, you saw nothing',
+				jp: '皆、何も見てない'
+			},
+			{
+				file: 'Hola Watamigo.mp3',
+				en: '¡Hola, Watamigo!',
+				jp: 'オラ！わたミーゴ！'
+			},
+			{
+				file: 'It_s me Watame.mp3',
+				en: "It's me, Watame (Mario style)",
+				jp: 'イツミー、わーため（マリオ風）'
+			},
+			{
+				file: 'Daijoubu dayo.mp3',
+				en: 'daijoubu dayo',
+				jp: '大丈夫だよ！'
+			},
+			{
+				file: 'Itterasshai.mp3',
+				en: 'itterasshai',
+				jp: 'いってらっしゃ～い'
+			},
+			{
+				file: 'Tsunomaki Janken Start.mp3',
+				en: 'Tsunomaki Janken, Start!',
+				jp: 'つのまきじゃんけん、スタート！'
+			},
+			{
+				file: 'Sheep noises.mp3',
+				en: 'Baa Baa',
+				jp: 'ばああ　ばああ'
+			},
+			{
+				file: 'Thigh drum.mp3',
+				en: 'Thigh drum',
+				jp: 'もも太鼓'
+			},
+			{
+				file: 'Warukunai.mp3',
+				en: 'Watame, did nothing wrong, riiight?',
+				jp: 'わため、悪くないよねぇ～？'
+			},
+			{
+				file: 'Watabeam.mp3',
+				en: 'Watabeeeam',
+				jp: 'わたビーーーム'
+			},
+			{
+				file: 'Watalaser.mp3',
+				en: 'Watalaser',
+				jp: 'わたレーザー'
+			},
+			{
+				file: 'Watame Factory (Acapella).mp3',
+				en: 'Watame Factory (Acapella)',
+				jp: 'わため工場（アカペラ）'
+			},
+			{
+				file: 'Watame laugh.mp3',
+				en: 'Laugh 1',
+				jp: '笑い声 1'
 			}
-		]
-	},
-	{
+		],
 		title: {
-			en: 'noises',
-			jp: 'ノイゼズ'
-		},
-		sounds: [
-			{
-				en: '15-shion wheeze_EN',
-				jp: '15-shion wheeze_JP',
-				file: '15-shion wheeze.mp3'
-			},
-			{
-				en: '16-shion wwww_EN',
-				jp: '16-shion wwww_JP',
-				file: '16-shion wwww.mp3'
-			},
-			{
-				en: '17-shion_doorbell_EN',
-				jp: '17-shion_doorbell_JP',
-				file: '17-shion_doorbell.mp3'
-			},
-			{
-				en: '18-shion_suki_EN',
-				jp: '18-shion_suki_JP',
-				file: '18-shion_suki.mp3'
-			},
-			{
-				en: '19-shion_unchi_EN',
-				jp: '19-shion_unchi_JP',
-				file: '19-shion_unchi.mp3'
-			},
-			{
-				en: '20-shionAHAHA_EN',
-				jp: '20-shionAHAHA_JP',
-				file: '20-shionAHAHA.mp3'
-			},
-			{
-				en: '20_goshujinsama_EN',
-				jp: '20_goshujinsama_JP',
-				file: '20_goshujinsama.mp3'
-			},
-			{
-				en: '3-kousui3 (laugh)_EN',
-				jp: '3-kousui3 (laugh)_JP',
-				file: '3-kousui3 (laugh).mp3',
-			},
-			{
-				en: '31_gugigubaba_EN',
-				jp: '31_gugigubaba_JP',
-				file: '31_gugigubaba.mp3',
-			},
-			{
-				en: '4-kousui4_EN',
-				jp: '4-kousui4_JP',
-				file: '4-kousui4.mp3'
-			}	
-		]
-	},
-	{
-		title: {
-			en: 'Other',
-			jp: 'アザー'
-		},
-		sounds: [
-			{
-				en: '40_haaaanya_EN',
-				jp: '40_haaaanya_JP',
-				file: '40_haaaanya.mp3',
-			},
-			{
-				en: '50_hanyanyaonib_EN',
-				jp: '50_hanyanyaonib_JP',
-				file: '50_hanyanyaonib.mp3',
-			},
-			{
-				en: '51_hanyaonya_EN',
-				jp: '51_hanyaonya_JP',
-				file: '51_hanyaonya.mp3',
-			},
-			{
-				en: '52_HANYA_EN',
-				jp: '52_HANYA_JP',
-				file: '52_HANYA.mp3',
-			},
-			{
-				en: '53_nyanyanyanya_EN',
-				jp: '53_nyanyanyanya_JP',
-				file: '53_nyanyanyanya.mp3',
-			},
-			{
-				en: '54_hanyanyanya_EN',
-				jp: '54_hanyanyanya_JP',
-				file: '54_hanyanyanya.mp3',
-			},
-			{
-				en: '55_hAnyanyanya_EN',
-				jp: '55_hAnyanyanya_JP',
-				file: '55_hAnyanyanya.mp3'
-			},
-			{
-				en: '55_hAnyanyanya_EN',
-				jp: '55_hAnyanyanya_JP',
-				file: '55_hAnyanyanya.mp3'
-			},
-			{
-				en: '60_UWAH_EN',
-				jp: '60_UWAH_JP',
-				file: '60_UWAH.mp3'
-			},
-			{
-				en: '61_KAWAII_EN',
-				jp: '61_KAWAII_JP',
-				file: '61_KAWAII.mp3'
-			},
-			{
-				en: '62_chyau_EN',
-				jp: '62_chyau_JP',
-				file: '62_chyau.mp3'
-			},
-			{
-				en: '63_kyon_EN',
-				jp: '63_kyon_JP',
-				file: '63_kyon.mp3'
-			},
-			{
-				en: '64_Kyoon_EN',
-				jp: '64_Kyoon_JP',
-				file: '64_Kyoon.mp3'
-			},
-			{
-				en: '7-shion aaa_EN',
-				jp: '7-shion aaa_JP',
-				file: '7-shion aaa.mp3'
-			},
-			{
-				en: '71_www_EN',
-				jp: '71_www_JP',
-				file: '71_www.mp3'
-			},
-			{
-				en: '8-shion aaaa_EN',
-				jp: '8-shion aaaa_JP',
-				file: '8-shion aaaa.mp3'
-			},
-			{
-				en: '80_na-nan-na-Nan_EN',
-				jp: '80_na-nan-na-Nan_JP',
-				file: '80_na-nan-na-Nan.mp3'
-			},
-			{
-				en: '90_pikachu_EN',
-				jp: '90_pikachu_JP',
-				file: '90_pikachu.mp3'
-			},
-			{
-				en: 'eh_EN',
-				jp: 'eh_JP',
-				file: 'eh.mp3'
-			},
-			{
-				en: 'gogobo_gobobobobo_EN',
-				jp: 'gogobo_gobobobobo_JP',
-				file: 'gogobo_gobobobobo.mp3'
-			},
-			{
-				en: 'guhu_EN',
-				jp: 'guhu_JP',
-				file: 'guhu.mp3'
-			},
-			{
-				en: 'kawaii_EN',
-				jp: 'kawaii_JP',
-				file: 'kawaii.mp3'
-			},
-			{
-				en: 'kimoo_EN',
-				jp: 'kimoo_JP',
-				file: 'kimoo.mp3'
-			},
-			{
-				en: 'shion chuki chuki daichuki_EN',
-				jp: 'shion chuki chuki daichuki_JP',
-				file: 'shion chuki chuki daichuki.mp3'
-			},
-			{
-				en: 'shion daishuki 1_EN',
-				jp: 'shion daishuki 1_JP',
-				file: 'shion daishuki 1.mp3'
-			},
-			{
-				en: 'shion e kawaii_EN',
-				jp: 'shion e kawaii_JP',
-				file: 'shion e kawaii.mp3'
-			},
-			{
-				en: 'shion eeee kawaii_EN',
-				jp: 'shion eeee kawaii_JP',
-				file: 'shion eeee kawaii.mp3'
-			},
-			{
-				en: 'shion i love you 1_EN', 
-				jp: 'shion i love you 1_JP',
-				file: 'shion i love you 1.mp3'
-			},
-			{
-				en: 'shion i love you 2_EN', 
-				jp: 'shion i love you 2_JP',
-				file: 'shion i love you 2.mp3'
-			},
-			{
-				en: 'shion i love you 3_EN', 
-				jp: 'shion i love you 3_JP',
-				file: 'shion i love you 3.mp3'
-			},
-			{
-				en: 'shion i love you 4_EN', 
-				jp: 'shion i love you 4_JP',
-				file: 'shion i love you 4.mp3'
-			},
-			{
-				en: 'shion kawaii 1_EN', 
-				jp: 'shion kawaii 1_JP',
-				file: 'shion kawaii 1.mp3'
-			},
-			{
-				en: 'shion kawaii 2_EN',
-				jp: 'shion kawaii 2_JP',
-				file: 'shion kawaii 2.mp3'
-			},
-			{
-				en: 'shion kawaii 3_EN',
-				jp: 'shion kawaii 3_JP',
-				file: 'shion kawaii 3.mp3'
-			}			
-		]
-	},
-	{
-		title: {
-			en: 'sneeze tskr',
-			jp: 'くしゃみ助かる'
-		},
-		sounds: [
-			{
-				en: 'Sneeze 1_EN',
-				jp: 'Sneeze 1_JP',
-				file: 'Sneeze 1.mp3'
-			},
-			{
-				en: 'Sneeze 10_EN',
-				jp: 'Sneeze 10_JP',
-				file: 'Sneeze 10.mp3'
-			},
-			{
-				en: 'Sneeze 11_EN',
-				jp: 'Sneeze 11_JP',
-				file: 'Sneeze 11.mp3'
-			},
-			{
-				en: 'Sneeze 12_EN',
-				jp: 'Sneeze 12_JP',
-				file: 'Sneeze 12.mp3'
-			},
-			{
-				en: 'Sneeze 13_EN',
-				jp: 'Sneeze 13_JP',
-				file: 'Sneeze 13.mp3'
-			},
-			{
-				en: 'Sneeze 14_EN',
-				jp: 'Sneeze 14_JP',
-				file: 'Sneeze 14.mp3'
-			},
-			{
-				en: 'Sneeze 15_EN',
-				jp: 'Sneeze 15_JP',
-				file: 'Sneeze 15.mp3'
-			},
-			{
-				en: 'Sneeze 16_EN',
-				jp: 'Sneeze 16_JP',
-				file: 'Sneeze 16.mp3'
-			},
-			{
-				en: 'Sneeze 17_EN',
-				jp: 'Sneeze 17_JP',
-				file: 'Sneeze 17.mp3'
-			},
-			{
-				en: 'Sneeze 18_EN',
-				jp: 'Sneeze 18_JP',
-				file: 'Sneeze 18.mp3'
-			},
-			{
-				en: 'Sneeze 19_EN',
-				jp: 'Sneeze 19_JP',
-				file: 'Sneeze 19.mp3'
-			},
-			{
-				en: 'Sneeze 2_EN',
-				jp: 'Sneeze 2_JP',
-				file: 'Sneeze 2.mp3'
-			},
-			{
-				en: 'Sneeze 3_EN',
-				jp: 'Sneeze 3_JP',
-				file: 'Sneeze 3.mp3'
-			},
-			{
-				en: 'Sneeze 4_EN',
-				jp: 'Sneeze 4_JP',
-				file: 'Sneeze 4.mp3'
-			},
-			{
-				en: 'Sneeze 5_EN',
-				jp: 'Sneeze 5_JP',
-				file: 'Sneeze 5.mp3'
-			},
-			{
-				en: 'Sneeze 6_EN',
-				jp: 'Sneeze 6_JP',
-				file: 'Sneeze 6.mp3'
-			},
-			{
-				en: 'Sneeze 7_EN',
-				jp: 'Sneeze 7_JP',
-				file: 'Sneeze 7.mp3'
-			},
-			{
-				en: 'Sneeze 8_EN',
-				jp: 'Sneeze 8_JP',
-				file: 'Sneeze 8.mp3'
-			},
-			{
-				en: 'Sneeze 9_EN',
-				jp: 'Sneeze 9_JP',
-				file: 'Sneeze 9.mp3'
-			}
-		]
+			en: 'Voices (work in progress)',
+			jp: '音声（仮）'
+		}
 	}
 ];
